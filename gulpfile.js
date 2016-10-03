c3bc97dcec501fe1d2dd27f12e1c1f5c80555203
eczn <1975845422@qq.com>
2016-10-04 00:48:39 +0800

    project info

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp');
+var webpack = require('gulp-webpack');
+var named = require('vinyl-named');
+var connect = require('gulp-connect');
+var uglify = require('gulp-uglify');
+
+gulp.task('connect', function(){
+	connect.server({
+		root: './src',
+		port: 8080,
+		livereload: true
+	});
+});
+
+var appList = ['app', 'router'];
+
+gulp.task('bundle', function(){
+	return gulp.src(mapFiles(appList, 'js'))
+				.pipe(named())
+				.pipe(webpack({
+					module: {
+						loaders: [{
+							test: /\.vue$/,
+							loader: 'vue'
+						}, {
+							test: /\.(png|jpg)$/,
+							loader: 'url'
+						}]
+					},
+					watch: true
+				}))
+				// .pipe(uglify())
+        		.pipe(gulp.dest('src/lib/'))
+				.pipe(connect.reload());
+});
+
+gulp.task('uglify', function() {
+    return gulp.src(['src/lib/js/fastclick.js'])
+    .pipe(uglify())
+    .pipe(gulp.dest('src/lib/js/*.min.js'));
+});
+
+function mapFiles(list, extname) {
+	return list.map(function (app){
+		return 'src/' + app + '.' + extname;
+	});
+};
+
+gulp.task('default', ['bundle', 'connect']);
