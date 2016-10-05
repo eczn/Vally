96c2447049d1e411532479c6cdf06a54031717d2
eczn <eczn@moebaka.com>
2016-10-06 01:25:41 +0800

    typeB btn

--- src/components/public/btn.vue
+++ src/components/public/btn.vue
@@ -9,20 +9,38 @@
 		cursor: pointer;
 	}
 	.btn-B {
+		display: inline-flex;
+		align-items: center;
+		justify-content: center;
+		font-family: Courier;
 		position: absolute;
 		top: 0;
-		font-size: .6rem;
+		font-size: 1rem;
+		height: 1rem;
+		width: 1rem;
+		/*width: calc(1rem - 6px);*/
+		/*height: calc(1rem - 6px);*/
 		border: 2px dashed #555;
-		/*padding: .3rem .5rem;*/
-		border-radius: 16px;
+
+		border-radius: 1rem;
 		cursor: pointer;
+	}
+	.btn-B > span {
+		width: .3rem;
+		height: .3rem; 
+		border-top: 4px solid #555;
+		border-left: 4px solid #555;
+		/*border-bottom: 4px solid transparent;*/
+		/*border-right: 4px solid transparent;*/
+		transform: rotate(-45deg);
+		transform-origin: 75% 25% ;
 	}
 </style>
 
 <template>
 	<div>
 		<span v-on:click="A_onClick" class="btn-A" v-if="btntype == 'A'">{{text}}</span>
-		<span v-on:click="A_onClick" class="btn-B" v-if="btntype == 'B'">{{text}}</span>
+		<span v-on:click="A_onClick" class="btn-B" v-if="btntype == 'B'"><span></span></span>
 	</div>
 </template>
 
