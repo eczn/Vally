5511a9299780b41078fe8e49e0631f44d1b77db8
eczn <eczn@moebaka.com>
2016-10-24 23:22:56 +0800

    lazy...

--- src/lib/js/appConfig.js
+++ src/lib/js/appConfig.js
@@ -3,7 +3,20 @@
 
 // var backEnd = "http://192.168.1.10/ProjectBuilding";
 // var backEnd = "http://localhost/ProjectBuilding"; 
-var backEnd = "http://localhost/ProjectBuilding/aVally/src/vally-backEnd"
+
+var HOST = window.location.href.split('#!')[0]; 
+
+
+var whereVallyEnd = "vally-backEnd/";
+
+// console.log(HOST+whereVallyEnd); 
+var backEnd = HOST+whereVallyEnd;
+
+if (HOST.indexOf("8080")){
+	backEnd = "http://localhost/ProjectBuilding/aVally/src/vally-backEnd/"; 
+}
+
+// var backEnd = "";
 
 var parser = new HyperDown(); 
 // 	mdAfterParse = parser.makeHtml( this.article.body );
