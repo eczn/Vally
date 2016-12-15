062cb79cf4860432e47b3428bfc77f90b3dd8338
eczn <eczn@moebaka.com>
2016-12-15 11:37:20 +0800

    add hightlight based on hljs

--- vally.js
+++ vally.js
@@ -1,10 +1,42 @@
 var template = require('art-template');
 var fs = require("fs"); 
+
+var hljs = require('highlight.js'); // https://highlightjs.org/
+
+// Actual default values
 var md = require('markdown-it')({
+	// highlight: function (str, lang) {
+	// 	if (lang && hljs.getLanguage(lang)) {
+	// 		try {
+	// 			return hljs.highlight(lang, str).value;
+	// 		} catch (__) {}
+	// 	}
+
+	// 	return ''; // use external default escaping
+	// },
+	highlight: function (str, lang) {
+		if (lang && hljs.getLanguage(lang)) {
+			try {
+				return '<pre class="hljs"><code>' +
+					hljs.highlight(lang, str, true).value +
+					'</code></pre>';
+			} catch (__) {}
+		}
+
+		return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+	},
 	html: true,
 	linkify: true,
 	typographer: true
 });
+
+// var md = require('markdown-it')({
+// 	html: true,
+// 	linkify: true,
+// 	typographer: true
+// });
+
+
 // var config = require('./config'); 
 var config = require('./config'); 
 
