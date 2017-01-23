9bbc489d79fc640348244c440714d25635bae71c
eczn <eczn@moebaka.com>
2017-01-23 16:38:48 +0800

    mark-vally img-intro, foot-info

--- work/static/js/markVally.js
+++ work/static/js/markVally.js
@@ -20,6 +20,14 @@ mark = (function($){
 			console.log("close")
 			imgDis.removeClass('opa1').addClass("display-none"); 
 		}); 
+
+		// $('#ii').after('<h1>Test!</h1>');
+		$('img').each(function(idx, elem){
+			// $(elem) === $(this)
+			var imgAlt = $(this).attr('alt'); 
+			var toAdd = '<mark class="img-intro">' + imgAlt + '</mark>'; 
+			$(this).after(toAdd); 
+		}); 
 	}
 	var lineCount = function(){
 		$('.md pre code').each(function(idx, elem){
@@ -34,12 +42,41 @@ mark = (function($){
 			$(this).html(targetHTML); 
 		});
 	}
+	var footInit = function(){
+		// collect sub tag to .foot container
+		var subs = []; 
+		$('sub').each(function(){
+			if (!$(this).attr('more')){
+				return; 
+			}
+
+			$(this).html('<a href="#vally-foot">[' + (subs.length+1) + ']</a>'); 
+			subs.push($(this).attr('more')); 
+		}); 
+
+		subs.forEach(function(elem, idx, its){
+			var temp = makeNode('li', elem); 
+			$('#vally-foot').append(temp);
+		}); 
+
+		if (subs.length == 0) {
+			$('#vally-foot').empty();
+		}
+		console.log(subs); 
+	}
+
+	var makeNode = function(name, content){
+		return '<'+name+'>'+content+'</'+name+'>'
+	}
 
 	return {
 		imgInit: imgInit,
-		lineCount: lineCount
+		lineCount: lineCount,
+		footInit: footInit
 	}
 })($); 
 
+
 mark.imgInit(); 
 mark.lineCount(); 
+mark.footInit(); 
