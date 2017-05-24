08937a11149dd090a6e35c49b94ade586d9f7d90
eczn <1975845422@qq.com>
2017-05-24 23:11:54 +0800

    line counter :: codeborad

--- tools/vblog.js
+++ tools/vblog.js
@@ -5,17 +5,35 @@ var path = require('path')
   , template = require('art-template')
   , _ = require('ramda')
   , hljs = require('highlight.js')
+  , alias = require('./alias')
   , config = require('../config')
-  , md = require('markdown-it')({
+
+hljs.configure({
+	useBR: true
+});
+
+var md = require('markdown-it')({
 	highlight: function (str, lang) {
+		lang = alias(lang); 
+
+		let line = str.split('\n').length - 2; 
+		let lefts = ['3em', '3.6em'];
+
+		var lineCount = new Array(line + 1).fill(0).reduce((acc, cur, idx) => {
+			idx = idx < 100 ? ('00' + idx.toString()).slice(-2) : idx; 
+
+			return acc + `<li>${idx}</li>`; 
+		}, '<div class="lines">') + '</div>'; 
+
+		var left = lefts[Math.floor((line + 1) / 100)] || lefts[1]; 
+
 		if (lang && hljs.getLanguage(lang)) {
 			try {
-				return '<pre class="hljs"><code>' +
-					hljs.highlight(lang, str, true).value +
-					'</code></pre>';
+				return `<pre style="padding-left: ${left}" class="hljs ${lang}">${lineCount}<code class="lang-name">${lang}</code><code>` +
+							hljs.highlight(lang, str, true).value +
+						`</code></pre>`;
 			} catch (__) { throw __; }
 		}
-
 		return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
 	},
 	html: true,
