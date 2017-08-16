0e38f8c00ca6062bb3165bce521e069100585e74
eczn <1975845422@qq.com>
2017-08-17 01:12:58 +0800

    index logger

--- index.js
+++ index.js
@@ -3,11 +3,14 @@
 const server = require('./tools/server'); 
 const collector = require('./tools/collector'); 
 const write = require('./tools/write'); 
+const logger = require('./tools/logger'); 
+const initLog = logger.bucket('INIT'.cyan); 
+
+let now = new Date(); 
+write().then(initSuc =>{
+	let end = new Date(); 
 
 
-// collector(); 
-
-
-write().then(
-	server
-); 
+	initLog(`Init Success, Time: ${end - now} ms`.yellow); 
+	server(); 
+}); 
