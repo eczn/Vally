e9d833c1f67dbaf0d7446b26ae94d2a31763705e
eczn <1975845422@qq.com>
2017-08-20 22:51:16 +0800

    process js file

--- tools/style.js
+++ tools/style.js
@@ -1,7 +1,33 @@
-// style.js
-function toLinkTag(cssFile){
-	return `<link rel="stylesheet" href="/css/${cssFile}">`
+// style.js 
+const path = require('path'); 
+
+function Style(file){
+	this.file = file; 
 }
+
+Style.create = function(file){
+	return new Style(file); 
+}
+
+Style.prototype.toTag = function(){
+	let ext = path.parse(this.file);
+
+	if (ext === '.js'){
+		return this.toJSTag(); 
+	} else {
+		return this.toLinkTag(); 
+	}
+}
+
+Style.prototype.toLinkTag = function(){
+	return `<link rel="stylesheet" href="/css/${this.file}">`
+}
+
+Style.prototype.toJSTag = function(){
+	return `<script src="${this.file}"></script>`; 
+}
+
+var item2tag = item => Style.create(item).toTag();
 
 let styleTable = {
 	home: [
@@ -18,10 +44,12 @@ let styleTable = {
 	]
 }
 
+let publicFiles = [
 
+].map(item2tag)
 
 module.exports = function(name){
-	let cssArr = styleTable[name]; 
+	let fileArr = styleTable[name]; 
 
-	return cssArr.map(toLinkTag).join(''); 
+	return fileArr.map(item2tag).join('') + publicFiles.join(''); 
 }
