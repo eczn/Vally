b8a8caa6bd83048a52cdff270f947645e53b43c7
eczn <1975845422@qq.com>
2017-09-01 01:04:06 +0800

    blog 页面的样式

---
+++ view/js/blog.js
@@ -0,0 +1,18 @@
+// blog.js
+require.config({
+	baseUrl: "/js",
+	paths: {
+		"jquery": "jquery.min", 
+		"nav": "nav"
+	}
+});
+
+define(['jquery', 'nav'], function($, nav){
+	$(function(){
+		setTimeout(function(){
+			nav.init(); 
+			// nav.open(); 
+		}, 500); 
+	}); 
+})
+
