c6a50b58eefb1874dac5036794b225d89a380e53
eczn <1975845422@qq.com>
2018-02-09 23:03:11 +0800

    [A] back2top

--- view/js/blog.js
+++ view/js/blog.js
@@ -4,14 +4,20 @@ require.config({
 	paths: {
 		"jquery": "jquery.min", 
 		"nav": "nav", 
-		"btns": "btns"
+		"btns": "btns", 
+		"Back": "back2top"
 	}
 });
 
-define(['jquery', 'nav', 'btns'], function($, nav, btns){
+define(['jquery', 'nav', 'btns', 'Back'], function($, nav, btns, Back){
 	$(function(){
 		btns.init(); 
+
+		var back = new Back('.back-2-top', window); 
+		back.init(); 
 		
+		console.log(back); 
+
 		setTimeout(function(){
 			nav.init();
 			// nav.open(); 
