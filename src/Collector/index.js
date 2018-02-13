6db95c6a3e6ddb081fc2d57f7fa509ea111e6b76
eczn <1975845422@qq.com>
2018-02-14 04:46:38 +0800

    [+] for query

---
+++ src/Collector/index.js
@@ -0,0 +1,21 @@
+// let fs = require('then-fs')
+const glob = require('globby')
+    , CONFIG = require('../config')
+    , CONFIG_PATH = CONFIG.path
+    , path = require('path')
+    , V = require('../V/blog')
+
+// glob 
+function collector(){
+    let vblogs = V.fromdir(CONFIG_PATH.blog); 
+
+    let vblogs_loading = vblogs.map(
+        v => v.fresh()
+    ); 
+
+    return Promise.all(vblogs_loading); 
+}
+
+// collector().then(console.log)
+
+module.exports = collector; 
