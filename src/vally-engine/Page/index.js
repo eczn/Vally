2e128d9e898a0987477001bfd37840d6feeea36b
eczn <1975845422@qq.com>
2018-05-01 23:00:02 +0800

    [+] init pages

--- src/vally-engine/Page/index.js
+++ src/vally-engine/Page/index.js
@@ -2,9 +2,16 @@ const { EventEmitter } = require('events')
     , path = require('path')
     , resolve = require('./resolve')
     , fs = require('fs')
+    , vallyCtx = require('../ctx'); 
 
 class Page extends EventEmitter {
+    /**
+     * @description Page 类构造函数
+     * @param { String } entry 入口 
+     */
     constructor(entry) {
+        super(); 
+
         this.entry = entry; 
         this.entryResolve = resolve(this.entry); 
 
@@ -20,9 +27,14 @@ class Page extends EventEmitter {
         let info = null; 
         try {
             info = require(this.entry); 
+
+            if (typeof info === 'function'){
+                info = info(vallyCtx); 
+            }
         } catch (err){
-            console.error('[ Error ] Page 错误: 未定义 index.json'); 
+            console.error('[ Error ] Page 错误: 未定义 index.js 或 index.js 执行错误'); 
             console.error('Page 位置: ', this.entry); 
+            console.error(err); 
             process.exit(1);
         }
 
--- src/vally-engine/index.js
+++ src/vally-engine/index.js
@@ -1,4 +1,9 @@
-const vallyCtx = require('./ctx'); 
 const Page = require('./Page')
+const INNER_PAGES = require('../vally-inner-pages')
+
+// 初始化页面 
+const PAGES = INNER_PAGES.map(page_entry => {
+    return new Page(page_entry); 
+}); 
 
 
